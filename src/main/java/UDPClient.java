eba94d9f4bcbbce9e6171724fcfc74004452152d
taxevader007 <santiagoespinosagiraldo1@gmail.com>
2024-09-03 08:24:17 -0500

    first commit

---
+++ src/main/java/UDPClient.java
@@ -0,0 +1,27 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPClient {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket clientSocket = new DatagramSocket();
+
+        InetAddress IPAddress = 
+        InetAddress.getByName("localhost");
+
+        byte[] sendData = "Hello, World!".getBytes();
+
+        DatagramPacket sendPacket = 
+        new DatagramPacket(
+            sendData, 
+            sendData.length, 
+            IPAddress, 
+            9876);
+
+        clientSocket.send(sendPacket);
+
+        clientSocket.close();
+
+    }
+    
+}
---
+++ src/main/java/UDPServer.java
@@ -0,0 +1,23 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class UDPServer {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket serverSocket = 
+                new DatagramSocket(9876);
+
+        byte[] receiveData = new byte[1024];
+
+        while (true) {
+            DatagramPacket receivePacket = 
+            new DatagramPacket(receiveData, receiveData.length);
+
+            serverSocket.receive(receivePacket);
+
+            String message = 
+            new String(receivePacket.getData());
+            System.out.println("RECEIVED: " + message);
+        }
+        
+    }
+}
